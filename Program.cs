f20347372db4ac5530049fa30523923e8b13ae77
Juan Pablo Villatoro <juan.villatoro@ujcv.edu.hn>
2025-02-06 23:58:17 -0600

    ejercicios subidos

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Laboratorio1
     {
         static void Main(string[] args)
         {
+            //Ejercicio 1
+
+            Console.WriteLine("Ingrese el primer número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número: ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el cuarto número: ");
+            int num4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de todos es");
+            Console.WriteLine(num1 + num2 + num3 + num4);
+            Console.WriteLine("El promedio de todos es");
+            Console.WriteLine((num1 + num2 + num3 + num4) / 4);
         }
     }
 }
